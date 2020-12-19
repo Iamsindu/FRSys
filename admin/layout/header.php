6c7abd9bdd09328a6075b702462d59c17dff1d37
Iamsindu <csitan.sindhu@gmail.com>
2020-12-19 18:23:16 +0545

    added links to header

--- admin/layout/header.php
+++ admin/layout/header.php
@@ -42,7 +42,7 @@
             <!-- LOGO -->
             <div class="topbar-left">
                 <div class="text-center">
-                    <a href="index.html" class="logo"><i class="mdi mdi-assistant"></i> Annex</a>
+                    <a href="index.php" class="logo"><i class="mdi mdi-assistant"></i> FRSys</a>
                     <!-- <a href="index.html" class="logo"><img src="assets/images/logo.png" height="24" alt="logo"></a> -->
                 </div>
             </div>
@@ -54,7 +54,7 @@
                         <li class="menu-title">Main</li>
 
                         <li>
-                            <a href="index.html" class="waves-effect">
+                            <a href="index.php" class="waves-effect">
                                 <i class="mdi mdi-airplay"></i>
                                 <span> Dashboard <span class="badge badge-pill badge-primary float-right">7</span></span>
                             </a>
@@ -63,16 +63,18 @@
                         <li class="has_sub">
                             <a href="javascript:void(0);" class="waves-effect"><i class="fa fa-user-circle"></i> <span> Admin </span> <span class="float-right"><i class="mdi mdi-chevron-right"></i></span></a>
                             <ul class="list-unstyled">
-                                <li><a href="advanced-highlight.html">Admin Form</a></li>
-                                <li><a href="advanced-rating.html">Admin Show Table</a></li>
+                                <li><a href="add_admin.php">Add Admin</a></li>
+                                <li><a href="show_admin.php">Show Admin</a></li>
                                 <!-- <li><a href="advanced-alertify.html">Alertify</a></li>
                                     <li><a href="advanced-rangeslider.html">Range Slider</a></li> -->
                             </ul>
                         </li>
 
-                        <li>
-                            <a href="calendar.html" class="waves-effect"><i class="fa fa-user-o"></i><span> User </span></a>
-                            <li><a href="advanced-highlight.html">User Form</a></li>
+                        <li class="has_sub">
+                            <a href="javascript:void(0);" class="waves-effect"><i class="fa fa-user-o"></i> <span> User </span> <span class="float-right"><i class="mdi mdi-chevron-right"></i></span></a>
+                            <ul class="list-unstyled">
+                                <li><a href="show_user.php">Show User</a></li>
+                            </ul>
                         </li>
 
                         <li class="menu-title">Components</li>
@@ -80,32 +82,17 @@
                         <li class="has_sub">
                             <a href="javascript:void(0);" class="waves-effect"><i class="mdi mdi-restaurant"></i> <span> Restaurant </span> <span class="float-right"><i class="mdi mdi-chevron-right"></i></span></a>
                             <ul class="list-unstyled">
-                                <li><a href="ui-buttons.html">Restaurant Form</a></li>
-                                <li><a href="ui-cards.html">Restaurant Discount Form</a></li>
-                                <!-- <li><a href="ui-tabs-accordions.html">Tabs &amp; Accordions</a></li>
-                                    <li><a href="ui-modals.html">Modals</a></li>
-                                    <li><a href="ui-images.html">Images</a></li>
-                                    <li><a href="ui-alerts.html">Alerts</a></li>
-                                    <li><a href="ui-progressbars.html">Progress Bars</a></li>
-                                    <li><a href="ui-dropdowns.html">Dropdowns</a></li>
-                                    <li><a href="ui-lightbox.html">Lightbox</a></li>
-                                    <li><a href="ui-navs.html">Navs</a></li>
-                                    <li><a href="ui-pagination.html">Pagination</a></li>
-                                    <li><a href="ui-popover-tooltips.html">Popover & Tooltips</a></li>
-                                    <li><a href="ui-badge.html">Badge</a></li>
-                                    <li><a href="ui-carousel.html">Carousel</a></li>
-                                    <li><a href="ui-video.html">Video</a></li>
-                                    <li><a href="ui-typography.html">Typography</a></li>
-                                    <li><a href="ui-sweet-alert.html">Sweet-Alert</a></li>
-                                    <li><a href="ui-grid.html">Grid</a></li> -->
+                                <li><a href="add_resturant.php">Add Restaurant</a></li>
+                                <li><a href="show_resturant.php">Show Restaurant</a></li>
                             </ul>
                         </li>
 
                         <li class="has_sub">
                             <a href="javascript:void(0);" class="waves-effect"><i class="mdi mdi-food"></i><span> Food  </span> <span class="float-right"><i class="mdi mdi-chevron-right"></i></span></a>
                             <ul class="list-unstyled">
-                                <li><a href="tables-datatable.html">Food Form</a></li>
-                                <li><a href="tables-basic.html">Food Discount Form</a></li>
+                                <li><a href="add_food.php">Add Food</a></li>
+                                <li><a href="show_food.php">Show Food</a></li>
+                                <li><a href="add_food_discount.php">Add Food Discount </a></li>
 
 
                             </ul>
@@ -114,8 +101,8 @@
                         <li class="has_sub">
                             <a href="javascript:void(0);" class="waves-effect"><i class="mdi mdi-menu"></i><span> Menu </span> <span class="float-right"><i class="mdi mdi-chevron-right"></i></span></a>
                             <ul class="list-unstyled">
-                                <li><a href="charts-morris.html">Menu Form</a></li>
-                                <li><a href="charts-morris.html">Menu Show Table</a></li>
+                                <li><a href="add_menu.php">Add Menu</a></li>
+                                <li><a href="show_menu.php">Show Menu</a></li>
                                 <!-- <li><a href="charts-chartist.html">Chartist Chart</a></li>
                                     <li><a href="charts-chartjs.html">Chartjs Chart</a></li>
                                     <li><a href="charts-flot.html">Flot Chart</a></li>
@@ -127,8 +114,8 @@
                         <li class="has_sub">
                             <a href="javascript:void(0);" class="waves-effect"><i class="mdi mdi-category"></i> <span> Category</span> <span class="float-right"><i class="mdi mdi-chevron-right"></i></span></a>
                             <ul class="list-unstyled">
-                                <li><a href="icons-material.html">Category Form</a></li>
-                                <li><a href="icons-material.html">Category Show Table</a></li>
+                                <li><a href="add_category.php">Add Category</a></li>
+                                <li><a href="show_category.php">Show Category </a></li>
                                 <!-- <li><a href="icons-ion.html">Ion Icons</a></li>
                                     <li><a href="icons-fontawesome.html">Font Awesome</a></li>
                                     <li><a href="icons-themify.html">Themify Icons</a></li>
@@ -140,25 +127,25 @@
                         <li class="has_sub">
                             <a href="javascript:void(0);" class="waves-effect"><i class="mdi mdi-"></i><span> Discount </span> <span class="float-right"><i class="mdi mdi-chevron-right"></i></span></a>
                             <ul class="list-unstyled">
-                                <li><a href="tables-basic.html">Discount Form</a></li>
-                                <li><a href="tables-basic.html">Discount Show Table</a></li>
+                                <li><a href="add_discount.php">Add Discount</a></li>
+                                <li><a href="show_discount.php">Show Discount</a></li>
                                 <!-- <li><a href="tables-datatable.html">Data Table</a></li>
                                     <li><a href="tables-responsive.html">Responsive Table</a></li>
                                     <li><a href="tables-editable.html">Editable Table</a></li> -->
                             </ul>
                         </li>
                         <li class="has_sub">
-                            <a href="javascript:void(0);" class="waves-effect"><i class="mdi mdi-table"></i><span> Similar Item </span> <span class="float-right"><i class="mdi mdi-chevron-right"></i></span></a>
+                            <a href="javascript:void(0);" class="waves-effect"><i class="mdi mdi-table"></i><span>Add Items</span> <span class="float-right"><i class="mdi mdi-chevron-right"></i></span></a>
                             <ul class="list-unstyled">
-                                <li><a href="tables-basic.html">Similar Item Add</a></li>
-                                <li><a href="tables-datatable.html">Special Item Add</a></li>
+                                <li><a href="add_similar_items.php">Add Similar Item</a></li>
+                                <li><a href="add_special_item.php">Add Special Item</a></li>
 
                             </ul>
                         </li>
                         <li class="has_sub">
                             <a href="javascript:void(0);" class="waves-effect"><i class="mdi mdi-bill"></i><span> Bill </span> <span class="float-right"><i class="mdi mdi-chevron-right"></i></span></a>
                             <ul class="list-unstyled">
-                                <li><a href="tables-basic.html">Bill Show Table</a></li>
+                                <li><a href="show-bill.php">Show Bill</a></li>
 
 
                             </ul>
@@ -178,13 +165,12 @@
                         <li class="has_sub">
                             <a href="javascript:void(0);" class="waves-effect"><i class="mdi mdi-google-pages"></i><span> Pages </span> <span class="float-right"><i class="mdi mdi-chevron-right"></i></span></a>
                             <ul class="list-unstyled">
-                                <li><a href="pages-login.html">Login</a></li>
-                                <li><a href="pages-register.html">Register</a></li>
-                                <li><a href="pages-recoverpw.html">Recover Password</a></li>
-                                <li><a href="pages-lock-screen.html">Lock Screen</a></li>
-                                <li><a href="pages-blank.html">Blank Page</a></li>
-                                <li><a href="pages-404.html">Error 404</a></li>
-                                <li><a href="pages-500.html">Error 500</a></li>
+                                <li><a href="pages-login.php">Login</a></li>
+                                <li><a href="pages-register.php">Register</a></li>
+                                <li><a href="pages-recoverpw.php">Recover Password</a></li>
+                                <li><a href="pages-lock-screen.php">Lock Screen</a></li>
+                                <li><a href="pages-404.php">Error 404</a></li>
+                                <li><a href="pages-500.php">Error 500</a></li>
                             </ul>
                         </li>
 
