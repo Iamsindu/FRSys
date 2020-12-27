a7475dba7ea77416f3be05373513afda3e45cd18
sndpokhrel <71160738+sndpokhrel@users.noreply.github.com>
2020-12-27 14:43:59 +0545

    admin class complete

--- admin/class/admin.class.php
+++ admin/class/admin.class.php
@@ -1,13 +1,13 @@
 <?php 
 class admin extends common
 {
-	public $admin_id,$username,$salt,$password,$email,$role,$date,$status;
+	public $admin_id,$username,$salt,$password,$email_id,$role,$date,$status;
 	public function selectadminbyusername()
 	{
 		$sql="select * from admin where username='$this->username'";
 		return $this->select($sql);
 	}
-	public function insertuser()
+	public function insertadmin()
 	{
 		$sql ="insert into admin(username,password,salt,role,email_id,date,status)values('$this->username','$this->password','$this->salt','$this->role','$this->email','$this->date','$this->status')";
 		return $this->insert($sql);
