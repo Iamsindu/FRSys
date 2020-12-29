bf93cecee3d52267ffd2a78b5fa3f79c9fae8346
sndpokhrel <71160738+sndpokhrel@users.noreply.github.com>
2020-12-29 08:46:55 +0545

    trimmed unnessery code

--- admin/add_category.php
+++ admin/add_category.php
@@ -12,12 +12,9 @@
     <link rel="shortcut icon" href="assets/images/favicon.ico">
     <!-- Plugins css -->
     <link href="assets/plugins/timepicker/tempusdominus-bootstrap-4.css" rel="stylesheet" />
-    <link href="assets/plugins/timepicker/bootstrap-material-datetimepicker.css" rel="stylesheet">
-    <link href="assets/plugins/clockpicker/jquery-clockpicker.min.css" rel="stylesheet" />
-    <link href="assets/plugins/colorpicker/asColorPicker.min.css" rel="stylesheet" type="text/css" />
+    
     <link href="assets/plugins/select2/select2.min.css" rel="stylesheet" type="text/css" />
-    <link href="assets/plugins/bootstrap-colorpicker/css/bootstrap-colorpicker.min.css" rel="stylesheet">
-    <link href="assets/plugins/bootstrap-datepicker/css/bootstrap-datepicker.min.css" rel="stylesheet">
+    
     <link href="assets/plugins/bootstrap-touchspin/css/jquery.bootstrap-touchspin.min.css" rel="stylesheet" />
     <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css">
     <link href="assets/css/icons.css" rel="stylesheet" type="text/css">
