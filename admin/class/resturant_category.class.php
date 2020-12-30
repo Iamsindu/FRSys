bd5ebb042e6149cb53010417a85105211e9a970a
sndpokhrel <71160738+sndpokhrel@users.noreply.github.com>
2020-12-30 19:15:49 +0545

    resturant category class added

--- admin/class/resturant_category.class.php
+++ admin/class/resturant_category.class.php
@@ -1,22 +1,17 @@
 <?php 
-class resturant extends common
+class resturant_category extends common
 {
-	public $rest_id, $rest_name, $phone_no, $password, $email_id, $date, $status, $open_time, $close_time, $delivery, $photo_id, $sim_id ;
-	public function insertresturant()
+	public $restcat_id,$rest_id,$cat_id ;
+	public function insertresturant_category()
 	{
-		$sql ="insert into resturant(rest_id,rest_name,phone_no,email_id,date,status,delivery)
-						=values('$this->rest_id','$this->rest_name','$this->phone_no','$this->email_id','$this->date', $this->status,$this->delivery)";
+		$sql ="insert into restcat(restcat_id,rest_id,cat_id)
+						=values('$this->restcat_id','$this->rest_id','$this->cat_id')";
 		return $this->insert($sql);
 	}
-	public function selectresturant()
+	public function selectresturant_category()
 	{
-		$sql= "select * from resturant";
+		$sql= "select * from restcat";
 		return $this->select($sql);
 	}
-	public function selectresturant_name()
-	{
-		$sql= "select rest_id rest_name from resturant";
-		return $this->select($sql);
-	}
-} 
+}
 ?>
