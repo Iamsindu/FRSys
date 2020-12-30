a4ea6c3b4e95498ae776c0b261362b4962ff3574
sndpokhrel <71160738+sndpokhrel@users.noreply.github.com>
2020-12-30 18:21:27 +0545

    fod category class added

---
+++ admin/class/food_category.class.php
@@ -0,0 +1,24 @@
+<?php 
+class food_category extends common
+{
+	public $foodcat_id, $food_id,$cat_id;
+ 	public function selectfood_category()
+ 	{
+ 		$sql = "select * from foodcat";
+ 		$data= $this->select($sql);
+ 		return $data; 
+ 	}
+ 	public function insertfood_category()
+ 	{
+
+		$sql ="insert into foodcat(foodcat_id,food_id,cat_id)values('$this->foodcat_id','$this->food_id','$this->cat_id')";
+ 		
+ 		return $this->insert($sql);
+ 	}
+
+ 
+
+ 	
+
+}
+?>
