9c2f7780e6cf863d5500683bd2f2ecbfaec4b05c
sndpokhrel <71160738+sndpokhrel@users.noreply.github.com>
2020-12-30 18:54:53 +0545

    payment class added

---
+++ admin/class/payment.class.php
@@ -0,0 +1,17 @@
+<?php 
+class payment extends common
+{
+	public $payment_id,$bill_id,$amount,$paymethod,$date;
+ 	public function selectpayment()
+ 	{
+ 		$sql = "select * from payment";
+ 		$data= $this->select($sql);
+ 		return $data; 
+ 	}
+ 	public function insertpayment()
+ 	{
+		$sql ="insert into payment(payment_id,bill_id,amount,paymethod,date)values('$this->payment_id','$this->bill_id','$this->amount','$this->paymethod','$this->date')";
+ 		return $this->insert($sql);
+ 	}
+}
+?>
