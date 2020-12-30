be89c646481dd06ed597bedf51f4639ef15aa8fc
Iamsindu <csitan.sindhu@gmail.com>
2020-12-30 19:06:01 +0545

    add_admin and add_discount validated and test completed

--- admin/add_admin.php
+++ admin/add_admin.php
@@ -26,116 +26,97 @@
 
 <body class="fixed-left">
     <?php
-    $i=1;     
-	require_once 'class/common.class.php';
-	require_once 'class/admin.class.php';
-	//require_once 'class/session.class.php';
-    //sessionhelper::checklogin();
-    //require_once 'selector.php';
-    //$a[2]=1;
-    // $note=[];
-    $username = $password = $email_id = $role = $status =  "";
-
-    require_once 'layout/header.php';
-    $admin=new admin; 
+        $i=1;     
+	    require_once 'class/common.class.php';
+	    require_once 'class/admin.class.php';
+	    //require_once 'class/session.class.php';
+        //sessionhelper::checklogin();
+        //require_once 'selector.php';
+        require_once 'layout/header.php';
     
-    //$err=[];
-    $err[1]=$err[2]=$err[3]=$err[4]=$err[5]="";
-	// 
-		// if (isset($_POST['username'])&& !empty($_POST['username']))
-		//  {
-            
-		// 	$admin->username= $_POST['username'];
-		// }
-		// else
-		// {
-		// 	$err[1]="Username must be Entered";
-        // }
+        $admin=new admin; 
+    
+        $username = $password = $email_id = $role = $status =  "";
+        $err[1]=$err[2]=$err[3]=$err[4]=$err[5]="";
+	
         if(isset($_POST['cmdsubmit'])){
-            echo "<br>"."hit";
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        if (empty($_POST["username"])) {
-            $err[1] = "Username is required";
-        } else {
-            $username = test_input($_POST["username"]);
-            // check if name only contains letters and whitespace
-            if (!preg_match("/^[a-zA-Z]+([a-zA-Z0-9](_|-| )[a-zA-Z0-9])*[a-zA-Z0-9]+$/",$username)) {
-              $err[1] = "Must begin with letters and only _,- and letters are allowed";
-            }
-        }
+            // echo "<br>"."hit";
+            if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                if (empty($_POST["username"])) {
+                    $err[1] = "Username is required";
+                } else {
+                    $username = test_input($_POST["username"]);
+                    // check if name only contains letters and whitespace
+                    if (!preg_match("/^[a-zA-Z]+([a-zA-Z0-9](_|-| )[a-zA-Z0-9])*[a-zA-Z0-9]+$/",$username)) {
+                        $err[1] = "Must begin with letters and only _,- and letters are allowed";
+                    }
+                }
           
         
-        if(empty($_POST["password"])){
-            $err[2] = "Password can't be empty.";
-        } else {
-            $password = test_input($_POST["password"]);
-            if (!preg_match("#.*^(?=.{8,20})(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*\W).*$#",$password)) {
-                $err[2] = "apply strong password please";
-            }
-        }
+                if(empty($_POST["password"])){
+                    $err[2] = "Password can't be empty.";
+                } else {
+                    $password = test_input($_POST["password"]);
+                    if (!preg_match("#.*^(?=.{8,20})(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*\W).*$#",$password)) {
+                        $err[2] = "apply strong password please";
+                    }
+                }
         
-        if (empty($_POST["email_id"])) {
-            $err[3] = "Email is required";
-        } else {
-            $email_id = test_input($_POST["email_id"]);
-            // check if e-mail address is well-formed
-            if (!filter_var($email_id, FILTER_VALIDATE_EMAIL)) {
-                $err[3] = "Invalid format and please re-enter valid email";
-            }
-        }
-
-        if (empty($_POST["role"])) {
-            $err[4] = "Role is required";
-        } else {
-            $role = test_input($_POST["role"]);
-        }
-
-        if (empty($_POST["status"])) {
-            $err[5] = "Status is required";
-        } else {
-            $status = test_input($_POST["status"]);
-        }
-    }
-		if($err[1]=="" && $err[2]=="" && $err[3]=="" &&  $err[4]=="" && $err[5]=="")  {
-            $data = $admin->selectadmin();
-            foreach ($data as $value)
-            {
-                $comp = strcmp($username,$value->username);
-            }
-            $admin->username = $username;
-            $admin->email_id = $email_id;
-            $admin->role =$role;
-            $admin->status = $status;
-            $admin->salt = uniqid();
-            $admin->date=date('Y-m-d H:i:s');
-			$admin->password= sha1($admin->salt.$password);
-			$ask =$admin->insertadmin();
-			if($ask==1)
-			{
-				echo "<script> alert('inserted successfully') </script>";
-			}	
-			else
-			{
-                if($comp!=0)
-                {
-                    echo "<script> alert('Failed to insert') </script>";
+                if (empty($_POST["email_id"])) {
+                    $err[3] = "Email is required";
+                } else {
+                    $email_id = test_input($_POST["email_id"]);
+                    // check if e-mail address is well-formed
+                    if (!filter_var($email_id, FILTER_VALIDATE_EMAIL)) {
+                        $err[3] = "Invalid format and please re-enter valid email";
+                    }
                 }
-                else
-                {
-                    echo "<script> alert('Duplicate') </script>";
+
+                if (empty($_POST["role"])) {
+                    $err[4] = "Role is required";
+                } else {
+                    $role = test_input($_POST["role"]);
                 }
-			}
-		}
-    }
+
+                if (empty($_POST["status"])) {
+                    $err[5] = "Status is required";
+                } else {
+                    $status = test_input($_POST["status"]);
+                }
+            }       
+
+		    if($err[1]=="" && $err[2]=="" && $err[3]=="" &&  $err[4]=="" && $err[5]=="")  {
+                $data = $admin->selectadmin();
+                foreach ($data as $value) {
+                    $comp = strcmp($username,$value->username);
+                }
+                $admin->username = $username;
+                $admin->email_id = $email_id;
+                $admin->role =$role;
+                $admin->status = $status;
+                $admin->salt = uniqid();
+                $admin->date=date('Y-m-d H:i:s');
+			    $admin->password= sha1($admin->salt.$password);
+			    $ask =$admin->insertadmin();
+			    if($ask==1){
+				    echo "<script> alert('Inserted admin Successfully') </script>";
+			    } else {
+                    if($comp!=0) {
+                        echo "<script> alert('Sorry! Failed to insert admin.') </script>";
+                    } else {
+                        echo "<script> alert('Duplicate admin value. Please insert unique admin!') </script>";
+                    }
+			    }
+		    }
+        }
     
-    function test_input($data) {
-        $data = trim($data);
-        $data = stripslashes($data);
-        $data = htmlspecialchars($data);
-        return $data;
-    }
-
- ?>	
+        function test_input($data) {
+            $data = trim($data);
+            $data = stripslashes($data);
+            $data = htmlspecialchars($data);
+            return $data;
+        }
+    ?>	
                 <div class="page-content-wrapper ">
                     <div class="container-fluid">
                         <div class="row">
@@ -161,31 +142,31 @@
                                             <div class="form-group">
                                                 <h6 class="text-muted fw-400">Username</h6>
                                                 <div>
-                                                    <input type="text" name="username" value="<?php echo $username;?>" class="form-control" required placeholder="Name" />
+                                                    <input type="text" name="username" value="<?php echo $username;?>" class="form-control" placeholder="Name" />
                                                     <span class="error"> <?php echo $err[1];?></span>
                                                 </div>
                                             </div>
                                             <div class="form-group">
                                                 <h6 class="text-muted fw-400">Password</h6>
                                                 <div>
-                                                    <input type="password" id="pass2" name="password" value="<?php echo $password;?>"  class="form-control" required placeholder="Password" />
+                                                    <input type="password" id="pass2" name="password" value="<?php echo $password;?>"  class="form-control" placeholder="Password" />
                                                     <span class="error"> <?php echo $err[2];?></span>
                                                 </div>
                                                 <div class="m-t-10">
-                                                    <input type="password" value="<?php echo $password;?>" class="form-control" required data-parsley-equalto="#pass2" placeholder="Re-Type Password" />
+                                                    <input type="password" value="<?php echo $password;?>" class="form-control" data-parsley-equalto="#pass2" placeholder="Re-Type Password" />
                                                     <span class="error"> <?php echo $err[2];?></span>
                                                 </div>
                                             </div>
                                             <div class="form-group">
                                                 <h6 class="text-muted fw-400">Email</h6>
                                                 <div>
-                                                    <input type="email" name="email_id" value="<?php echo $email_id; ?>" class="form-control" required placeholder="Enter email" />
+                                                    <input type="email" name="email_id" value="<?php echo $email_id; ?>" class="form-control" placeholder="Enter email" />
                                                     <span class="error"> <?php echo $err[3];?></span>
                                                 </div>
                                             </div>
                                             <div class="form-group">
                                                 <h6 class="text-muted fw-400">Role</h6>
-                                                <select class="select2 form-control custom-select" style="width: 100%; height:36px;" name="role" required>
+                                                <select class="select2 form-control custom-select" style="width: 100%; height:36px;" name="role">
                                                     <option disabled selected>Select</option>
                                                     <option>Admin</option>
                                                     <option>Editor</option>
@@ -195,11 +176,12 @@
                                             </div>
                                             <div class="form-group">
                                                 <h6 class="text-muted fw-400">Status</h6>
-                                                <select class="select2 form-control custom-select" style="width: 100%; height:36px;" name="status" required >
+                                                <select class="select2 form-control custom-select" style="width: 100%; height:36px;" name="status">
                                                     <option disabled selected>Select</option>
                                                     <option>Active</option>
                                                     <option>Inactive</option>
                                                 </select>
+                                                <span class="error"> <?php echo $err[5]; ?> </span> 
                                             </div>
                                             <div class="form-group ">
                                                 <div>
