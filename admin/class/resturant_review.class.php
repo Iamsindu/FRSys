6f4d2e6d3eb5c95d354950da112f388f7e20eec9
sndpokhrel <71160738+sndpokhrel@users.noreply.github.com>
2020-12-30 19:43:09 +0545

    resturant review class added

---
+++ admin/class/resturant_review.class.php
@@ -0,0 +1,16 @@
+<?php 
+class resturant_review extends common
+{
+	public $reviewr_id, $user_id, $rest_id, $dsc, $date ;
+	public function insertresturant_review()
+	{
+		$sql ="insert into reviewr(reviewr_id, user_id, rest_id, dsc, date) values('$this->reviewr_id','$this->user_id','$this->rest_id','$this->dsc','$this->date')";
+		return $this->insert($sql);
+	}
+	public function selectresturant_review()
+	{
+		$sql= "select * from reviewr";
+		return $this->select($sql);
+	}
+}
+?>
