6eb3613497ed1043b87f4615dd28119075d33906
sndpokhrel <71160738+sndpokhrel@users.noreply.github.com>
2020-12-30 19:42:11 +0545

    special class added

---
+++ admin/class/special.class.php
@@ -0,0 +1,16 @@
+<?php 
+class special extends common
+{
+	public $spcl_id, $rest_id, $food_id, $dsc, $date ;
+	public function insertspecial()
+	{
+		$sql ="insert into spcl(spcl_id, rest_id, food_id, dsc, date) values('$this->spcl_id','$this->rest_id','$this->food_id','$this->dsc','$this->date')";
+		return $this->insert($sql);
+	}
+	public function selectspecial()
+	{
+		$sql= "select * from spcl";
+		return $this->select($sql);
+	}
+}
+?>
