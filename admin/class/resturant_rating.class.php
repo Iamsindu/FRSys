10aa39786ca71f3dca209b01416561aa92bdae93
sndpokhrel <71160738+sndpokhrel@users.noreply.github.com>
2020-12-30 19:42:53 +0545

    resturant rating class added

---
+++ admin/class/resturant_rating.class.php
@@ -0,0 +1,16 @@
+<?php 
+class resturant_rating extends common
+{
+	public $rrate_id, $rest_id, $user_id, $rating, $date ;
+	public function insertresturant_rating()
+	{
+		$sql ="insert into rrate(rrate_id, rest_id, user_id, rating, date) values('$this->rrate_id','$this->rest_id','$this->user_id','$this->rating','$this->date')";
+		return $this->insert($sql);
+	}
+	public function selectresturant_rating()
+	{
+		$sql= "select * from rrate";
+		return $this->select($sql);
+	}
+}
+?>
