dbb7c5fd21bbf71360a0d1f1e391a43b682f50dc
sndpokhrel <71160738+sndpokhrel@users.noreply.github.com>
2020-12-30 18:12:14 +0545

    disimilar class added

---
+++ admin/class/dissimilar.class.php
@@ -0,0 +1,17 @@
+<?php 
+class dissimilar extends common
+{
+	public $dis_id,$food_id,$rest_id;
+	
+	public function insertdissimilar()
+	{
+		$sql ="insert into dissimilar(food_id,rest_id) values ('$this->food_id','$this->rest_id')";
+		return $this->insert($sql);
+	}
+	public function selectdissimilar()
+	{
+		$sql= "select * from dissimilar";
+		return $this->select($sql);
+	} 
+} 
+?>
