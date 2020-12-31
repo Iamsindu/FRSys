383470d4f911c8ca720f78a51e82f9eaab9c47fd
sndpokhrel <71160738+sndpokhrel@users.noreply.github.com>
2020-12-31 13:43:02 +0545

    solved resturant faild insert

--- admin/class/resturant.class.php
+++ admin/class/resturant.class.php
@@ -4,7 +4,7 @@ class resturant extends common
 	public $rest_id, $rest_name, $phone_no, $password, $email_id, $date, $status, $open_time, $close_time, $delivery, $photo_id, $sim_id ;
 	public function insertresturant()
 	{
-		$sql ="insert into resturant(rest_id,rest_name,phone_no,email_id,date,status,delivery)values('$this->rest_id','$this->rest_name','$this->phone_no','$this->email_id','$this->date', $this->status,$this->delivery)";
+		$sql ="insert into resturant(rest_id,rest_name,phone_no,password,salt,email_id,date,status,open_time,close_time,delivery)values('$this->rest_id','$this->rest_name','$this->phone_no','$this->password','$this->salt','$this->email_id','$this->date','$this->status','$this->open_time','$this->close_time','$this->delivery')";
 		return $this->insert($sql);
 	}
 	public function selectresturant()
@@ -20,59 +20,5 @@ class resturant extends common
 } 
 ?>
 
-<?php
- class product extends common{
- 	public $id,$name,$price,$cat,$picture,$disc;
-
- 	public function selectproduct()
- 	{
- 		$sql = "select * from tbl_product";
- 		$data= $this->select($sql);
- 		return $data; 
- 	}
-
- 	public function selectproductbyid()
- 	{
- 		$sql = "select * from tbl_product where id = '$this->id' ";
- 		return $this->select($sql);
- 	}
-
- 	public function insertproduct()
- 	{
-
- 		$sql = "insert into tbl_product(name,price,cat_name,picture,disc)values('$this->name','$this->price','$this->cat','$this->picture','$this->disc') ";
- 		
- 		return $this->insert($sql);
- 	}
-
- 	public function insertwithoutimg()
- 	{
- 		 $sql = "insert into tbl_product(name,price,cat_name,disc)values('$this->name','$this->price','$this->cat','$this->disc')";
- 	
- 		return $this->insert($sql);
- 	}
-
- 	public function deleteproduct()
- 	{
- 		$sql = "delete from tbl_product where id = '$this->id' ";
- 		return $this->delete($sql);
- 	}
-
- 	public function updateproduct()
- 	{
-
- 		if(!empty($this->picture))
- 		{
- 			$sql = "update tbl_product set name = '$this->name',price = '$this->price',cat_name = '$this->cat',picture = '$this->picture',disc = '$this->disc' where id='$this->id'";
- 		}
-	 	else	
-	 	{
-	 		$sql = "update tbl_product set name = '$this->name',price = '$this->price',cat_name = '$this->cat',disc = '$this->disc' where id='$this->id'";
-	 	}
-	 	return $this->update($sql);
-	 }
-
-}
-?>
 
 
