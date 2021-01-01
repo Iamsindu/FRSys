f27fa0f85ff37d5f98215f1056c0225d92092550
Iamsindu <csitan.sindhu@gmail.com>
2021-01-01 21:25:52 +0545

    added update and delete sql on classes

--- admin/class/category.class.php
+++ admin/class/category.class.php
@@ -2,34 +2,33 @@
 class category extends common
 {
 	public $cat_id,$catname;
-	// public function selectcategorybytype()
-	// {
-	// 	$sql="select * from cat where catname='$this->catname'";
-	// 	return $this->select($sql);
-	// }
 	public function insertcategory()
 	{
 		$sql ="insert into cat(catname) values ('$this->catname')";
 		return $this->insert($sql);
 	}
+
 	public function selectcategory()
 	{
 		$sql= "select * from cat";
 		return $this->select($sql);
 	}
+
 	public function selectcategorybyid()
  	{
  		$sql = "select * from cat where cat_id = '$this->cat_id' ";
  		return $this->select($sql);
- 	}
+	 }
+	 
 	public function updatecategory()
  	{
-	 	$sql = "update admin set username = '$this->username',role = '$this->role',email_id = '$this->email_id',date = '$this->date',status = '$this->status' where admin_id='$this->admin_id'";
+	 	$sql = "update cat set catname = '$this->catname' where cat_id='$this->cat_id'";
 	 	return $this->update($sql);
 	}
+	
 	public function deletecategory()
  	{
- 		$sql = "delete from admin where admin_id = '$this->admin_id' ";
+ 		$sql = "delete from cat where cat_id = '$this->cat_id' ";
  		return $this->delete($sql);
  	}
 } 
