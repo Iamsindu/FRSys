824faac30acc8db1b281f0931546c0c814ef6fda
Iamsindu <csitan.sindhu@gmail.com>
2021-04-18 22:26:59 +0545

    sign up

--- account.php
+++ account.php
@@ -1,5 +1,83 @@
 <?php
 	require_once 'layout/next_header.php';
+	require_once '../class/common.class.php';
+	require_once '../class/user.class.php'
+    
+	$name = $password = $email_id = $phone_no = "";
+	$user =new user;
+	$err[1]=$err[2]=$err[3]=$err[4]="";
+
+	function test_input($info) {
+		$info = trim($info);
+		$info = stripslashes($info);
+		$info = htmlspecialchars($info);
+		return $info;
+	}
+
+    if(isset($_POST['signup'])) 
+	{
+		if ($_SERVER["REQUEST_METHOD"] == "POST") 
+		{
+			if (empty($_POST["name"])) {
+				$err[1] = "Name is required";
+			} else {
+				$username = test_input($_POST["username"]);
+				if (!preg_match("/^[a-zA-Z]+([a-zA-Z0-9](_|-| )[a-zA-Z0-9])*[a-zA-Z0-9]+$/",$name)) {
+					$err[1] = "Must begin with letters and only _,- and letters are allowed";
+				}
+			}
+
+			if (empty($_POST["email_id"])) {
+				$err[2] = "Email is required";
+			} else {
+				$email_id = test_input($_POST["email_id"]);
+				if (!filter_var($email_id, FILTER_VALIDATE_EMAIL)) {
+					$err[2] = "Invalid format and please re-enter valid email";
+				}
+			}
+	
+			if (empty($_POST["phone_no"])) {
+				$err[3] = "phone_no is required";
+			} else {
+				$phone_no = test_input($_POST["phone_no"]);
+			}
+			
+			if(empty($_POST["password"])){
+				$err[4] = "Password can't be empty.";
+			} else {
+				$password = test_input($_POST["password"]);
+				if (!preg_match("#.*^(?=.{8,20})(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*\W).*$#",$password)) {
+					$err[4] = "apply strong password please";
+					$password="";
+				}
+			}
+		}   
+		
+		if($err[1]=="" && $err[2]=="" && $err[3]=="" &&  $err[4]=="")  
+		{
+			$data = $user->selectusers();
+			foreach ($data as $value) {
+				$comp = strcmp($name,$value->name);
+			}
+			$user->name = $name;
+			$user->email_id = $email_id;
+			$user->phone_no=$phone_no;
+			$user->salt = uniqid();
+			$user->password= sha1($user->salt.$password);
+		
+			$ask =$user->insertuser();
+			if($ask==1){
+				echo "<script> alert('Inserted user Successfully') </script>";
+				echo '<script> window.location="confirm.php" </script>';
+			} else {
+				if($comp!=0) {
+					echo "<script> alert('Sorry! Failed to insert user.') </script>";
+				} else {
+					echo "<script> alert('Duplicate user value. Please insert unique user!') </script>";
+				}
+			}
+    	}
+	}
 ?>
 	
 	<main class="bg_gray pattern">
@@ -19,24 +97,30 @@
 						<div class="divider"><span>Or</span></div> -->
 		                <!-- <h6>Personal details</h6> -->
 		                <div class="form-group">
-		            		<input class="form-control" placeholder="First and Last Name">
+		            		<input class="form-control" placeholder="First and Last Name" name="name">
 		            			<i class="icon_pencil"></i>
 		            	</div>
 
 		            	<div class="form-group">
-		            		<input class="form-control" placeholder="Email Address">
+		            		<input class="form-control" placeholder="Email Address" name="email_id">
 		            		<i class="icon_mail"></i>
 		            	</div>
 						<div class="form-group">
-		            		<input class="form-control" placeholder="Contact">
+		            		<input class="form-control" placeholder="Contact" name="phone_no">
 		            		<i class="icon_phone"></i>
 		            	</div>
 		            	<div class="form-group add_bottom_15">
-		            		<input class="form-control" placeholder="Password" id="password_sign" name="password_sign">
+		            		<input class="form-control" placeholder="Password" id="password_sign" name="password">
 		            		<i class="icon_lock"></i>
 		            	</div>
+						
+						<div class="form-group text-center row m-t-20">
+                            <div class="col-12">
+                                <button class="btn_1 full-width mb_5" type="submit" name="signup">Sign up Now</button>
+                            </div>
+                        </div>
 
-		                <a href="confirm.php" class="btn_1 full-width mb_5">Sign up Now</a>
+		                <!-- <a href="confirm.php" class="btn_1 full-width mb_5">Sign up Now</a> -->
 		            </div>
 		            </div>
 		        </div>
